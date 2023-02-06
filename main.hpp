59171472d355a7f9fb872b2d8238de6235f9f3a5
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-02-06 13:04:31 +0100

    Removed code

--- main.hpp
+++ main.hpp
@@ -1,8 +1,4 @@
 #ifndef MAIN_HPP_
 #define MAIN_HPP_
 
-// Profiling
-//#include <gperftools/profiler.h>
-#define BREAKPOINT(MSG) do { puts(MSG); fflush(stdout); while (getchar() != '\n'); } while (0)
-
 #endif
