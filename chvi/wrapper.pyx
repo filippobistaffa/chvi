30317e3687040e12de73ba3cbf005c5327330166
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-02-08 12:27:53 +0100

    Better code

--- chvi/wrapper.pyx
+++ chvi/wrapper.pyx
@@ -14,10 +14,10 @@ cdef public cpp_vector[size_t] observation_space_size(env):
     return env.observation_space.nvec
 
 cdef public size_t execute_action(env, cpp_vector[size_t] state, size_t action):
-    print(f"Received state: {state} (type: {type(state)})")
-    print(f"Received action: {action} (type: {type(action)})")
     assert env.observation_space.contains(state), f"State {state} not part of the observation space"
     assert env.action_space.contains(action), f"Action {action} not part of the action space"
+    print(f"Received state: {state} (type: {type(state)})")
+    print(f"Received action: {action} (type: {type(action)})")
     #env.reset(state)
     #next_state, rewards, d, _ = env.step(action)
     return id(env)
