bd2df5839b5978d46e5bcf5f65dc86c67c00c297
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-02-09 11:02:13 +0100

    Comment

--- chvi/wrapper.pyx
+++ chvi/wrapper.pyx
@@ -25,7 +25,7 @@ cdef public cpp_vector[size_t] get_observation_space_size(env):
 cdef public cpp_vector[double] get_action_rewards(env, cpp_vector[size_t] state, size_t action):
     assert env.observation_space.contains(state), f"State {state} not part of the observation space"
     assert env.action_space.contains(action), f"Action {action} not part of the action space"
-    env.reset()
+    env.reset() # not sure if needed
     env.state = env.unwrapped.state = np.array(state) # probably not the most memory-optimized way
     _, rewards, _, _ = env.step(action)
     return np.atleast_1d(rewards)
