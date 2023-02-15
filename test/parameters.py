3b3b41fd92483c373ba6e3b5e0fc7d9cdc27e27c
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-02-15 11:41:56 +0100

    Updated test parameters

--- test/parameters.py
+++ test/parameters.py
@@ -1,15 +1,15 @@
 parameters = {
     # environment parameters
-    "max_dimensions": 3,
-    "max_space_size": 4,
-    "max_actions": 6,
+    "max_dimensions": 6,
+    "max_space_size": 11,
+    "max_actions": 10,
     # algorithm parameters
     "discount_factor": 1.0,
-    "max_iterations": 3,
+    "max_iterations": 100,
     "epsilon": 0.0,
     # tests parameters
-    "seed": 0,
-    "seed_list": [356965412],
-    "n_tests": 1000,
+    "seed": 123,
+    #"seed_list": [356965412],
+    "n_tests": 10,
     "max_seed": 2**32 - 1
 }
