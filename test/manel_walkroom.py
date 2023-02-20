977b2c5d2034e30663574d40a612e86bb2d10ba3
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-02-20 14:51:14 +0100

    Removed useless code

--- test/manel_walkroom.py
+++ test/manel_walkroom.py
@@ -3,21 +3,6 @@ import itertools
 
 from gymnasium.spaces import Box, Discrete, MultiDiscrete
 from gymnasium.core import Env
-from manel_convexhull import get_hull
-
-
-def non_dominated(solutions, return_indexes=False):
-    is_efficient = np.ones(solutions.shape[0], dtype=bool)
-    for i, c in enumerate(solutions):
-        if is_efficient[i]:
-            # Remove dominated points, will also remove itself
-            is_efficient[is_efficient] = np.any(solutions[is_efficient] > c, axis=1)
-            # keep this solution as non-dominated
-            is_efficient[i] = 1
-    if return_indexes:
-        return solutions[is_efficient], is_efficient
-    else:
-        return solutions[is_efficient]
 
 
 def initializeGrid(size, dimensions, random_seed):
@@ -37,9 +22,6 @@ class WalkRoom(Env):
         self.size = size
         self.dimensions = dimensions
         self.room = self.make_boundaries(seed=seed)
-        # pareto_front_i = [c + (self.room[c],) for c in (np.unravel_index(i, self.room.shape) for i in nd_i)]
-        # self.pareto_front_b = np.array(pareto_front_i)*-1
-        # possible actions, do not move in diagonal
         self.actions = np.zeros((self.dimensions*2, self.dimensions), dtype=int)
         # move forward
         self.actions[range(self.dimensions), range(self.dimensions)] = 1
@@ -52,18 +34,11 @@ class WalkRoom(Env):
         self.reward_space = Box(-np.inf, 0, (dimensions,))
         self.reset()
 
-    @property
-    def pareto_front(self):
-        d = self.dimensions-1
-        boundary = [coord + (self.room[coord],) for coord in itertools.product(*(np.arange(self.size),)*d)]
-        pf = non_dominated(np.array(boundary)*-1)
-        return pf
-
     def make_boundaries(self, seed=0):
         d = self.dimensions-1
         room = initializeGrid(self.size, d, seed)
         room = room.reshape((self.size,)*d)
-        #print("The room: ", room)
+        print("The room:\n", room)
         return room
         
     def reset(self, forced_pos=None):
@@ -74,7 +49,6 @@ class WalkRoom(Env):
 
     def step(self, action):
         pos = self.pos + self.actions[action]
-        # stay in room bounds
         pos = np.clip(pos, 0, self.size-1)
         reward = -np.abs(self.actions[action].copy())
         terminal = self.is_terminal(pos)
@@ -121,22 +95,5 @@ class WalkRoom(Env):
         return np.sum(state[self.dimensions-j] * self.size**(j-1) for j in range(1, self.dimensions + 1))
 
 
-def random_walkroom(size, jumps=2, seed=0):
-    rng = np.random.default_rng(seed)
-    # make sure that, on average you have one jump per step
-    p = np.ones(jumps)* 1/np.arange(jumps).sum()
-    p[0] = 1-(jumps-1)*p[1]
-    y = rng.choice(np.arange(jumps), size=size, p=p).cumsum()[::-1]
-    return WalkRoom(size, y)
-
-
-def voxel_representation(env, s, d):
-    import matplotlib.pyplot as plt
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    voxels = np.zeros((s,)*d, dtype=bool)
-    for coord in itertools.product(*(np.arange(s),)*(d-1)):
-        if 0 < env.room[coord] < env.size:
-            voxels[coord+(env.room[coord],)] = True
-    ax.voxels(voxels)
-    plt.show()
+if __name__ == "__main__":
+    env = WalkRoom(4, 3, 1)
