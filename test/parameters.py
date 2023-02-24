3e70fc49262871c6d33984f8bd3b6d6bc3c7c89f
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-02-24 10:47:48 +0100

    Update parameters.py

--- test/parameters.py
+++ test/parameters.py
@@ -2,13 +2,13 @@ import sys
 
 parameters = {
     # environment parameters
-    "max_dimensions": 3,
-    "max_size": 3,
+    "max_dimensions": 6,
+    "max_size": 6,
     "goals": 0.01,
     # algorithm parameters
     "discount_factor": 1.0,
     "max_iterations": 100,
-    "epsilon": 0.0,
+    "epsilon": 0.05,
     # tests parameters
     "seed": 12345,
     "n_tests": 100,
