ec58f8d586f8ff00e305d157daa5f9bbd66a20b9
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-03-20 16:10:34 +0100

    Better profiling

--- chvi/profile.sh
+++ chvi/profile.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/env bash
 
 bin="./chvi"
-qcachegrind="./qcachegrind"
 pprof="google-pprof"
 trace="trace"
-heapcheck="normal" #strict draconian
 
-if [[ $1 != "cpu" && $1 != "leak" ]]
-then
-	echo "Usage: $0 [cpu|leak] arguments"
-	exit
-fi
-
-if [[ $1 == "cpu" ]]
-then
-        ${bin} ${@:2}
-        ${pprof} --callgrind ${bin} ${trace}.prof > ${trace}.callgrind
-        ${qcachegrind} ${trace}.callgrind &
-else
-        env PPROF_PATH=`which ${pprof}` HEAPCHECK=${heapcheck} ${bin} ${@:2}
-fi
+${bin} $*
+${pprof} --gv ${bin} ${trace}.prof
